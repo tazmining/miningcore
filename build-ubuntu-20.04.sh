653a0615180763008c17505655ec1a6951887c94
TazMining <support@tazmining.ch>
2024-10-15 12:55:11 +0200

    Update build-ubuntu-20.04.sh / add build 1

--- build-ubuntu-20.04.sh
+++ build-ubuntu-20.04.sh
@@ -11,9 +11,9 @@ rm packages-microsoft-prod.deb
 
 # install dev-dependencies
 sudo apt-get update; \
-  sudo apt-get -y install dotnet-sdk-6.0 git cmake clang ninja-build build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev libzmq5 libgmp-dev libc++-dev
+  sudo apt-get -y install dotnet-sdk-6.0 git cmake ninja-build build-essential libssl-dev pkg-config libboost-all-dev libsodium-dev libzmq5 libgmp-dev
 
 (cd src/Miningcore && \
-BUILDIR=${1:-../../build} && \
+BUILDIR=${1:-../../build1} && \
 echo "Building into $BUILDIR" && \
 dotnet publish -c Release --framework net6.0 -o $BUILDIR)
